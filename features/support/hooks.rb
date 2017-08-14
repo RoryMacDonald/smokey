d3e9698331c7b1c16f3d2be33b0fbf29612381d7
Alec Gibson <aleckgibson@googlemail.com>
2017-08-14 16:10:34 +0100

    WIP

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,3 +5,8 @@ end
 Before('@withanalytics') do
   page.driver.browser.url_blacklist = BLACKLISTED_URLS - [GOOGLE_ANALYTICS_URL]
 end
+
+After do |scenario|
+  ENV['EMAIL_RECIPIENTS'] = 'alec.gibson@digital.cabinet-office.gov.uk'
+  Cucumber.wants_to_quit = true if scenario.failed? # TODO: Remove this line
+end
